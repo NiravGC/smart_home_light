c1a57d3593086b4207bb5bed6861c0758d29a71a
Nirav <32882197+NiravGC@users.noreply.github.com>
2018-09-29 17:44:30 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# NOTES ON CONFIGURATION
+# Strip is made up of three controllable sections; left, middle, and bar.
+# LEFT section is the first 24 pixels
+# MIDDLE section is pixels 25 - 74
+# BAR section is pixels 75 - ??
