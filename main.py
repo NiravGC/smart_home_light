cb873436e59b78a694f0d988844a09104e93664a
Nirav <32882197+NiravGC@users.noreply.github.com>
2018-09-30 17:53:13 +0100

    update comment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Strip is made up of three controllable sections; left, middle, and bar.
 # LEFT section is the first 24 pixels
 # MIDDLE section is pixels 25 - 74
-# BAR section is pixels 75 - ??
+# BAR section is pixels 75 - 132
 
 import time
 import logging
